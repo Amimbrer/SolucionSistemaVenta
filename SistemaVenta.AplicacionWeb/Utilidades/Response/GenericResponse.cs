067c4e3f9a70951f18aeba13f4d518b4389b6075
Amimbrer <136268910+Amimbrer@users.noreply.github.com>
2023-06-29 02:47:12 +0200

    Subida video 6

---
+++ SistemaVenta.AplicacionWeb/Utilidades/Response/GenericResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace SistemaVenta.AplicacionWeb.Utilidades.Response
+{
+    public class GenericResponse<TObject>
+    {
+        public bool Estado { get; set; }
+
+        // la interrogacion permite que se pueda asignar valores nulos
+        public string? Mensaje { get; set; } 
+        public TObject? Objeto { get; set; } 
+        public List<TObject>? ListaObjeto { get; set; }
+
+
+
+    }
+}
